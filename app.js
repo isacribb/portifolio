2f1f8a812a84602badf6e5117de5532c57069813
Isac Brito <103973686+isacribb@users.noreply.github.com>
2023-01-18 23:34:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const projetosNav = document.querySelector('#projetos-nav');
+const tecnologiasNav = document.querySelector('#tecnologias-nav');
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+// click
+projetosNav.addEventListener('click', () => {
+    window.scroll({ top: window.scrollTo(0, 1100), behavior: "smooth" })
+})
+
+tecnologiasNav.addEventListener('click', () => {
+    window.scroll({ top: window.scrollTo(0, 580), behavior: "smooth" })
+})
+
+// scroll
+function animeScroll() {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.8)
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+        element.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', function(){
+        animeScroll();
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
